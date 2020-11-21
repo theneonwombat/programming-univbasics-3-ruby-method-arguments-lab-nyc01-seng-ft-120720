902e44cce2e3a2ec8c9141295991dae942abc934
Alec Caro <alec.caro@gmail.com>
2020-11-21 17:04:29 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,7 +4,7 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 def introduction(name)
-  puts "Hi, my name is " + name
+  puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language(name, language)
@@ -12,7 +12,7 @@ def introduction_with_language(name, language)
 end
 
 def introduction_with_language_optional(name, language = "Ruby")
-  puts "Hi, my name is " + name + " and I am learning to program in" + language + "."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 
